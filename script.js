6ea05c5f67121904be185b2f9a05be2801ecf78d
Vladislavovna2407 <Vladislavovna2018@bk.ru>
2022-11-13 17:48:45 +0100

    Домашнее задание 5

--- script.js
+++ script.js
@@ -13,3 +13,43 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+function doProbability(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+let list = [
+    "Тебе сегодня напишет тот, от которого ждешь сообщения",
+    "Сегодня тебя ждет чудесный день",
+    "Если ты ждала знака, то это он",
+    "Ты заслуживашь все лайки инстаграма",
+    "Ты со всем справишься, ты всегда со всем справлялась",
+    "Ты найдешь решение задачи, которая тебя давно мучает",
+    "Тот милый бариста приготовит бесплатный кофе"
+];
+
+let prediction = null;
+let probability = null;
+
+const button = document.querySelector('.forecast-btn');
+button.addEventListener('click', function() {
+
+    if (prediction != null) {
+        const template = document.querySelector('#forecast-item');
+        const clone = template.content.cloneNode(true);
+        clone.querySelector('h3').textContent = prediction;
+        clone.querySelector('p').textContent = `Вероятность: ${probability}%`;
+        const container = document.querySelector('.forecasts');
+        container.prepend(clone);
+    }
+
+
+    prediction = list[Math.floor(Math.random() * 7)];
+    probability = doProbability(0, 100);
+
+    document.querySelector('.current-forecast h1').textContent = prediction;
+    document.querySelector('.current-forecast p').textContent = `Вероятность: ${probability}%`;
+
+
+
+})
